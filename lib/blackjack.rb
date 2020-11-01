f62993fbc04b4a3de642e559e0ef84b8df9f6898
Alexander Bowers <7319abowers@gmail.com>
2020-11-01 09:21:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,9 +61,6 @@ def runner
   card_total = initial_round
   until card_total > 21
     hit?(card_total)
-    while hit?(card_total)
-      hit?(card_total)
-    end
     display_card_total(card_total)
   end
 end_game
